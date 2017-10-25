fbd0c7a1417a50f6c2901087e5f3e4c2d8ec4ea9
Kagami <moe.pascal@gmail.com>
2017-10-25 22:07:42 +0800

    update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,12 +1,19 @@
-// this requires node^7.6
-
 const fetch = require('node-fetch')
 const fs = require('fs-extra')
 const path = require('path')
+const HttpsProxyAgent = require('https-proxy-agent')
+
+const proxy = process.env.https_proxy || process.env.http_proxy || ''
+if (proxy) {
+  console.log('using proxy', proxy)
+}
 
 const main = async () => {
   try {
-    const resp = await fetch('https://kcwikizh.github.io/kcdata/quest/poi.json')
+    const resp = await fetch('https://kcwikizh.github.io/kcdata/quest/poi.json', {
+      agent: proxy ? new HttpsProxyAgent(proxy) : null,
+    })
+    console.log('fetched')
     const content = await resp.json()
     await fs.outputJSON(path.resolve(__dirname, './assets/info/index.json'), content, { spaces: 2 })
   } catch (e) {
